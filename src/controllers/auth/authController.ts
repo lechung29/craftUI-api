9e1e31926cfe9dadd22462476275799b688ca3d9
Killian Le <86923962+lechung29@users.noreply.github.com>
2026-03-25 10:27:46 +0700

    Update authController.ts

--- src/controllers/auth/authController.ts
+++ src/controllers/auth/authController.ts
@@ -389,7 +389,9 @@ const requestPasswordRecovery: RequestHandler = async (req: Request, res: Respon
 
     try {
         const transporter = nodemailer.createTransport({
-            service: "gmail",
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,   
             auth: {
                 user: process.env.MY_EMAIL,
                 pass: process.env.MY_PASSWORD,
